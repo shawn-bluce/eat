f1eaffd7ae66ced5fe45bbba449c14e16873e328
Tomilla <Tomilla@users.noreply.github.com>
2024-07-06 22:25:23 +0800

    perfxmemory): split request large memory to multiple small-size chunks

--- cmd/constant.go
+++ cmd/constant.go
@@ -8,4 +8,5 @@ const (
 	intervalCpuWorkerCheckContextDone = 10000
 	durationMemoryWorkerDoRefresh     = 5 * time.Minute
 	durationEachSignCheck             = 100 * time.Millisecond
+	chunkSizeMemoryWokerEachAllocate  = 128 * 1024 * 1024 // 128MB
 )
