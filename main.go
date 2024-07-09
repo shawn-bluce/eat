fdd743fb5b80c6f86d1a113c54e42a9f4566b39c
Tomilla <Tomilla@users.noreply.github.com>
2024-07-09 15:15:48 +0800

    feat(cpu_affinity): add definition and parse for cmd IntSlice args `cpu-affinities`

--- main.go
+++ main.go
@@ -11,12 +11,13 @@ func main() {
 	rootCmd := cmd.RootCmd
 
 	// Add global flags
-	rootCmd.PersistentFlags().StringP("cpu_usage", "c", "0", "How many cpu would you want eat")
-	rootCmd.PersistentFlags().StringP("memory_usage", "m", "0m", "How many memory would you want eat(GB)")
+	rootCmd.PersistentFlags().StringP("cpu-usage", "c", "0", "How many cpu would you want eat")
+	rootCmd.PersistentFlags().IntSlice("cpu-affinities", []int{}, "Which cpu core(s) would you want to eat? multiple cores separate by ',' (start from 0)")
+	rootCmd.PersistentFlags().StringP("memory-usage", "m", "0m", "How many memory would you want eat(GB)")
 	// such as "300ms", "1.5h", "2h45m". (unit: "ns", "us" (or "µs"), "ms", "s", "m", "h")
-	rootCmd.PersistentFlags().StringP("time_deadline", "t", "0", "Deadline to quit eat process")
-	// same unit as time_deadline
-	rootCmd.PersistentFlags().StringP("memory_refresh_interval", "r", "5m", "How often to trigger a refresh to prevent the ate memory from being swapped out")
+	rootCmd.PersistentFlags().StringP("time-deadline", "t", "0", "Deadline to quit eat process")
+	// same unit as time-deadline
+	rootCmd.PersistentFlags().StringP("memory-refresh-interval", "r", "5m", "How often to trigger a refresh to prevent the ate memory from being swapped out")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
